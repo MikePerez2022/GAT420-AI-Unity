c93fef34938018de791855f78955506014542503
MikePerez2022 <123500465+MikePerez2022@users.noreply.github.com>
2024-01-09 15:33:21 -0700

    made physics move and obit cam script

---
+++ Assets/Scripts/AIAutonomusAgent.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIAutonomusAgent : Agent
+{
+    public AIPerception perception = null;
+
+    private void Update()
+    {
+        //
+    }
+}
